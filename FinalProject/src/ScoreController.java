856a1c404e935057b1953cbf3a9740ef4d47c52e
Zachary <Zachary@LAPTOP-I4C23HJ6>
2016-11-27 13:49:00 -0500

    Deleted unused class files.

--- FinalProject/src/ScoreController.java
+++ FinalProject/src/ScoreController.java
@@ -1,4 +1,14 @@
 
 public class ScoreController {
     
+    ScoreModel sModel;
+    ScoreView sView;
+    
+    ScoreController(ScoreModel sModel, ScoreView sView) {
+        
+        this.sModel = sModel;
+        this.sView = sView;
+        
+    }
+    
 }
