802acf807c4f092ad3e6cae4cb9e23efb185490b
Zachary <Zachary@LAPTOP-I4C23HJ6>
2016-11-27 13:26:19 -0500

    Just cleaned code up a bit.

--- FinalProject/src/AboutPanel.java
+++ FinalProject/src/AboutPanel.java
@@ -1,27 +1,17 @@
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class AboutPanel extends JPanel{
     
     private JLabel aboutTitle;
-    private JButton backButton;
     
     AboutPanel() {
         
         aboutTitle = new JLabel("About Menu");
-        backButton = new JButton("Back");
 
         add(aboutTitle);
-        add(backButton);
         
-    }
-    
-    public JButton getBackButton() {
-
-        return this.backButton;
-
     }
     
 }
--- FinalProject/src/CreditPanel.java
+++ FinalProject/src/CreditPanel.java
@@ -1,27 +1,17 @@
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CreditPanel extends JPanel{
     
     private JLabel creditTitle;
-    private JButton backButton;
     
     CreditPanel() {
         
         creditTitle = new JLabel("Credit Menu");
-        backButton = new JButton("Back");
 
         add(creditTitle);
-        add(backButton);
         
     }
     
-    public JButton getBackButton() {
-
-        return this.backButton;
-
-    }
-        
 }
