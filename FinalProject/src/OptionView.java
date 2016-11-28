95b184e1f4b5156722ceef1205185f513a3a0b00
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-28 15:08:13 -0500

    Added option GUI components.

--- FinalProject/src/OptionView.java
+++ FinalProject/src/OptionView.java
@@ -1,20 +1,37 @@
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 public class OptionView extends JPanel{
     
-    OptionModel oModel;
+    private OptionModel oModel;
     
-    JLabel optionLabel;
+    private JLabel optionLabel;
+    private JLabel colorLabel;
+    private JButton color1Button;
+    private JButton color2Button;
+    private JLabel difficultyLabel;
+    private JSlider difficultySlider;
 
     OptionView(OptionModel oModel) {
         
         this.oModel = oModel;
 
-        optionLabel = new JLabel("OPTIONS");
+        optionLabel = new JLabel("Options Menu");
+        colorLabel = new JLabel("Change Snake Color");
+        color1Button = new JButton("Color 1");
+        color2Button = new JButton("Color 2");
+        difficultyLabel = new JLabel("Difficulty");
+        difficultySlider = new JSlider(JSlider.HORIZONTAL, 1, 3, 1);
         
         add(optionLabel);
+        add(colorLabel);
+        add(color1Button);
+        add(color2Button);
+        add(difficultyLabel);
+        add(difficultySlider);
 
         // It may be useful to have a Save & Return button
 
