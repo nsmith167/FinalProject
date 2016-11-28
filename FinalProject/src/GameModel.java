5a755df54edd434fac7cb6497484808574c17ed8
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-28 15:27:29 -0500

    Added option GUI components.

--- FinalProject/src/GameModel.java
+++ FinalProject/src/GameModel.java
@@ -1,9 +1,15 @@
 
+import java.awt.Color;
+
 public class GameModel {
+    
+    private Color colorValue;
+    private Integer diffValue;
     
     GameModel() {
         
-        
+        colorValue = Color.GREEN;
+        diffValue = 0;
         
     }
     
