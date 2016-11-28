1b6dc6c039730cd0db2f84135fe4fe16d3f1e246
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-28 15:47:40 -0500

    Added option GUI components.

--- FinalProject/src/OptionView.java
+++ FinalProject/src/OptionView.java
@@ -1,5 +1,7 @@
 
 import java.awt.GridLayout;
+import java.util.Hashtable;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,30 +12,48 @@ public class OptionView extends JPanel{
     private OptionModel oModel;
     
     private JLabel optionLabel;
+    
     private JLabel colorLabel;
     private JButton color1Button;
     private JButton color2Button;
+    
     private JLabel difficultyLabel;
     private JSlider difficultySlider;
+    private Hashtable<Integer, JLabel> labelTable;
+    
+    private JPanel optionPane;
 
     OptionView(OptionModel oModel) {
         
         this.oModel = oModel;
+        
+        optionPane = new JPanel();
+        optionPane.setLayout(new GridLayout(6, 1));
+        
         optionLabel = new JLabel("Options Menu");
+        
         colorLabel = new JLabel("Change Snake Color");
         color1Button = new JButton("Color 1");
         color2Button = new JButton("Color 2");
+        
         difficultyLabel = new JLabel("Difficulty");
         difficultySlider = new JSlider(JSlider.HORIZONTAL, 1, 3, 1);
         difficultySlider.setMajorTickSpacing(1);
         difficultySlider.setPaintTicks(true);
+        difficultySlider.setPaintLabels(true);
+        labelTable = new Hashtable<>();
+        labelTable.put(1, new JLabel("Easy"));
+        labelTable.put(2, new JLabel("Medium"));
+        labelTable.put(3, new JLabel("Hard"));
+        difficultySlider.setLabelTable(labelTable);
         
-        add(optionLabel);
-        add(colorLabel);
-        add(color1Button);
-        add(color2Button);
-        add(difficultyLabel);
-        add(difficultySlider);
+        optionPane.add(optionLabel);
+        optionPane.add(colorLabel);
+        optionPane.add(color1Button);
+        optionPane.add(color2Button);
+        optionPane.add(difficultyLabel);
+        optionPane.add(difficultySlider);
+        add(optionPane);
         
     }
     
