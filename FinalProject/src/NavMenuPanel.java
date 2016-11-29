60b270af4b3776bfd45a0bd8c55055e64b30dc35
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-29 16:10:15 -0500

    Made buttons a consistent size Added method to make further changes to all buttons at once

--- FinalProject/src/NavMenuPanel.java
+++ FinalProject/src/NavMenuPanel.java
@@ -1,32 +1,55 @@
 
+import java.awt.Dimension;
+import java.util.ArrayList;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class NavMenuPanel extends JPanel {
 
-    //His example does not make these private so that they can be accessed elsewhere
+    Dimension menuButtonDimension;
+    
     JButton gameButton;
     JButton scoreButton;
     JButton optionButton;
     JButton aboutButton;
     JButton creditButton; 
+    
+    private ArrayList<JButton> buttons;
 	
     public NavMenuPanel() {
 	
         super();
+        
+        menuButtonDimension = new Dimension(150, 25);
+        
         gameButton = new JButton("Game");
         scoreButton = new JButton("Scores");
         optionButton = new JButton("Options");
         aboutButton = new JButton("Instructions");
         creditButton = new JButton("Credits");
-                                  
-        add(gameButton);
-        add(scoreButton);
-        add(optionButton);
-        add(aboutButton);
-        add(creditButton);		
+        
+        buttons = new ArrayList<>();
+        buttons.add(gameButton);
+        buttons.add(scoreButton);
+        buttons.add(optionButton);
+        buttons.add(aboutButton);
+        buttons.add(creditButton);
+        
+        applyButtonChanges();
+                   
+        this.add(gameButton);
+        this.add(scoreButton);
+        this.add(optionButton);
+        this.add(aboutButton);
+        this.add(creditButton);		
         
     }   
+    
+    //Method made for easier changes to all buttons
+    private void applyButtonChanges() {
+        for(int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setPreferredSize(menuButtonDimension);
+        }
+    }
     
 }
