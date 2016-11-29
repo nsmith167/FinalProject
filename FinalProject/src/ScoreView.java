cc6cbc48473c04b8dd3ec85d4ea37b04b3398cdd
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-29 16:12:20 -0500

    Cleaned up some code

--- FinalProject/src/ScoreView.java
+++ FinalProject/src/ScoreView.java
@@ -13,13 +13,10 @@ public class ScoreView extends JPanel{
     private ScoreModel sModel;
     
     private JPanel scorePane;
-    private JPanel searchPane;
-    
-    private JLabel scoreTitle;
-    
     private JLabel highScoreLabel;
     private JTextArea highScoreList;
     
+    private JPanel searchPane;
     private JLabel searchLabel;
     private MaskFormatter searchBoxFormat;
     private JFormattedTextField searchBox;
@@ -28,28 +25,32 @@ public class ScoreView extends JPanel{
     ScoreView(ScoreModel sModel) {
         
         this.sModel = sModel;
+        this.setLayout(new GridLayout(1, 2));
         
-        setLayout(new GridLayout(1, 2));
-        
-        scorePane = new JPanel();   //Will change layout of both panes later
-        searchPane = new JPanel();
-        
-        scoreTitle = new JLabel("Score Menu");
+        //Display Highest Scores
+        scorePane = new JPanel();
         
         highScoreLabel = new JLabel("High Scores");
+        
         highScoreList = new JTextArea("XYZ\t1000");
         highScoreList.setBackground(this.getBackground());
         highScoreList.setEditable(false);
-        scorePane.add(highScoreLabel);
+        
+        scorePane.add(highScoreLabel);  //Will change layout later
         scorePane.add(highScoreList);
         
+        //Search
+        searchPane = new JPanel();
+        
         searchLabel = new JLabel("Search:");
+        
         try {
             searchBoxFormat = new MaskFormatter("***");
         } catch(ParseException pe) {
             System.out.println("ERR: ParseException");
         }
         searchBoxFormat.setValidCharacters("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        
         searchBox = new JFormattedTextField(searchBoxFormat);
         searchBox.setColumns(3);
         searchBox.setHorizontalAlignment(JTextField.CENTER);
@@ -57,12 +58,13 @@ public class ScoreView extends JPanel{
         searchResult = new JTextArea("XYZ\t1000");
         searchResult.setBackground(this.getBackground());
         searchResult.setEditable(false);
-        searchPane.add(searchLabel);
+        
+        searchPane.add(searchLabel);    //Will change layout later
         searchPane.add(searchBox);
         searchPane.add(searchResult);
         
-        add(scorePane);
-        add(searchPane);
+        this.add(scorePane);
+        this.add(searchPane);
         
     }
     
