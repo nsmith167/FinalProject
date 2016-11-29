ecab6bb3472b4a646c62b5414aa2464d90deb9be
zri5004 <zri5004@202-WD09.up.ist.local>
2016-11-29 16:11:45 -0500

    Made the splash image more easily resizable

--- FinalProject/src/SplashPanel.java
+++ FinalProject/src/SplashPanel.java
@@ -1,4 +1,5 @@
 
+import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -7,12 +8,24 @@ import javax.swing.JPanel;
 public class SplashPanel extends JPanel{
     
     private JLabel splashLabel;
+    
+    private ImageIcon splashIcon;
+    private Image splashImage;
+    private Image splashNewImage;
+    
     private JButton splashButton;
     
     SplashPanel() {
 
-        splashLabel = new JLabel("WELCOME TO THE SNAKE GAME");
-        splashButton = new JButton(new ImageIcon("src/tiny_snek.jpg"));            
+        splashLabel = new JLabel("Welcome to the Snake Game");  //Maybe put this in the picture later on to make it look better
+        
+        splashIcon = new ImageIcon("src/tiny_snek.jpg");
+        splashImage = splashIcon.getImage();
+        splashNewImage = splashImage.getScaledInstance(780, 780,  java.awt.Image.SCALE_SMOOTH);
+        splashIcon = new ImageIcon(splashNewImage);
+        
+        splashButton = new JButton(splashIcon);
+        
         add(splashLabel);
         add(splashButton);
             
