7c3939b219aa85e7433104aa11fdfe4a8526ca68
zri5004 <zri5004@202-WD09.up.ist.local>
2016-12-07 15:37:17 -0500

    Visual changes

--- FinalProject/src/OptionController.java
+++ FinalProject/src/OptionController.java
@@ -4,22 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-
-public class OptionController
-{
+public class OptionController {
+    
     private OptionModel oModel;
     private OptionView oView;
     
-    OptionController(OptionModel oModel, OptionView oView) 
-    {
+    OptionController(OptionModel oModel, OptionView oView) {
+        
         this.oModel = oModel;
         this.oView = oView;
         
-        class ButtonListener implements ActionListener
-        {
+        class ButtonListener implements ActionListener {
             @Override
-            public void actionPerformed(ActionEvent ae)
-            {
+            public void actionPerformed(ActionEvent ae) {
                 JButton eventButton = (JButton)ae.getSource();
                 if(eventButton == oView.getColor1Button())
                     oModel.setSnakeColor(new Color(102, 222, 128));
@@ -34,5 +31,7 @@ public class OptionController
         }
         
         oView.addButtonListener(new ButtonListener());
+        
     }
+    
 }
